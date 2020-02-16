9c0e9c08e953088bb8d2ed24e9d025a939d14a18
dellajiang <isjiangyuantong@outlook.com>
2020-02-16 22:46:42 +0800

    0

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>xx论坛</title>
+
+</head>             
+<div class="top">
+<div class="text">
+      <div class="wenzi"><span style="display:inline-block"><a>首页</a>  ｜  <a>聊天室</a>  ｜  <a>我的主页</a> 
+
+    <form method="post" action="get" >
+      <dt>用户名<input type="text" name="username" class="px vm" tabindex="901" style="width:95px;" />
+       密 码 <input type="password" name="password" class="px vm" tabindex="902" style="width:95px;" />
+
+       <button type="submit" tabindex="904"><em>登录</em></button>
+       <button type="submit" tabindex="904"><em>注册</em></button>
+     </dt>
+</div></span>
+      <input type="hidden" name="quickforward" value="yes" />
+      <input type="hidden" name="handlekey" value="ls" />
+      
+    </form>    
+        
+</div>
+</div>
+
+
+</body>
+</html>
+
+
+
+
